def436d1d017b2dedefa89c2fd87f20170dfa26e
Jari Moellenbernd <jari.moellenbernd@lhind.dlh.de>
2018-05-31 07:24:21 +0200

    Changed logging

--- src/app/shared/change-detection-log.component.ts
+++ src/app/shared/change-detection-log.component.ts
@@ -3,6 +3,6 @@ import { OnChanges, SimpleChanges } from '@angular/core';
 export class ChangeDetectionLogComponent implements OnChanges {
   constructor(private name: string) {}
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(`Component ${this.name} changed ${changes}`);
+    console.log(`Component ${this.name} changed`, changes);
   }
 }
